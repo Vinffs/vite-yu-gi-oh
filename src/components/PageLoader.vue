84473557117b9e5cf393091f3866d5483db9700b
Vinffs <vin.demiranda@gmail.com>
2023-11-13 18:33:50 +0100

    deleted HeaderComponent and built PagePreloader

---
+++ src/components/PageLoader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="PageLoader">
+
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.pageLoader {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 10000;
+  background-color: blue;
+}
+</style>
